8bfd36e3f7ef9b68117b285513923758c1639be5
kamila <kamila.lach1@gmail.com>
2021-11-11 12:41:54 +0100

    Finish page layouts

---
+++ index.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const button = document.querySelector(".mobile-nav-toggle span");
+
+navToggle.addEventListener("click", () => {
+  const visibility = nav.getAttribute("data-visible");
+  if (visibility === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
