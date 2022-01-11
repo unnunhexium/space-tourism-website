22170a5bd50afc3fe640ce636ff26f6252c6c9c6
kamila <kamila.lach1@gmail.com>
2022-01-11 11:33:15 +0100

    Change layout

---
+++ technology.js
@@ -0,0 +1,31 @@
+const technologyTabs = document.querySelectorAll(".small-button");
+
+technologyTabs.forEach((tab) => {
+  tab.addEventListener("click", selectTechnology);
+});
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute("hidden");
+}
+
+function selectTechnology(e) {
+  let targetTab;
+  if (e.target.tagName === "SPAN") {
+    targetTab = e.target.parentNode;
+  } else {
+    targetTab = e.target;
+  }
+  const targetImage = targetTab.getAttribute("data-image");
+  const targetPanel = targetTab.getAttribute("aria-controls");
+
+  hideContent(document, ".technology-info");
+  showContent(document, [`#${targetPanel}`]);
+  hideContent(document, "picture");
+  showContent(document, [`#${targetImage}`]);
+}
